5367ee842f7555d2fdd41c1db51e29fdfc5b74fd
PondokProgrammer <afaizi1999@gmail.com>
2020-10-16 07:35:09 +0700

    Delete body-parser and change to express.json

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
 // node_modules
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
 // database
 const db = require("./queries");
 
-app.use(bodyParser.json());
-app.use(
-	bodyParser.urlencoded({
-		extended: true
-	})
-);
+app.use(express.json());
 
 // routes
 // home
 app.get("/", (request, response) => {
-	response.json({info: "Node.js, Express, and Postgres API"});
+	response.json({ info: "Node.js, Express, and Postgres API" });
 });
 // get all users
 app.get("/users", db.getUsers);
